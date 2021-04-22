968bbc3ab545bc1eff071102dbe0fd55b0c329ca
Natan <natan.barioni@sou.unifeob.edu.br>
2021-04-22 20:10:18 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,124 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { 
+  View, 
+  KeyboardAvoidingView, 
+  Image, 
+  StyleSheet, 
+  TextInput, 
+  TouchableOpacity, 
+  Text,
+  Animated,
+} from 'react-native';
 
 export default function App() {
+
+  const [offset] = useState(new Animated.ValueXY({x: 0, y: 80}));
+  const [opacity] = useState(new Animated.Value(0));
+
+  useEffect(() => {
+    Animated.parallel([
+      Animated.spring(offset.y, {
+        toValue: 0,
+        speed: 4,
+        bounciness: 20,
+        useNativeDriver:true
+      }),
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: 200,
+        useNativeDriver:true
+      })
+    ]).start();
+    
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView style={styles.background}>
+      <View style={styles.containerLogo}>
+        <Image 
+        source={require('./src/images/logo.png')}
+        />
+      </View>
+
+      <Animated.View 
+        style={[
+          styles.container,
+          {
+            opacity: opacity,
+            transform:[
+              { translateY: offset.y}
+            ]
+          }
+        ]}>
+        <TextInput 
+        style={styles.input}
+        placeholder="Email"
+        autoCorrect={false}
+        onChangeText={()=> {}}
+        />
+
+        <TextInput 
+        style={styles.input}
+        placeholder="Senha"
+        autoCorrect={false}
+        onChangeText={()=> {}}
+        />
+
+        <TouchableOpacity style={styles.btnSubmit}>
+          <Text style={styles.submitText}>Acessar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btnRegister}>
+          <Text style={styles.registerText}>Criar conta gratuita</Text>
+        </TouchableOpacity>
+      </Animated.View>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background:{
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#191919'
   },
+  containerLogo:{
+    flex: 1,
+    justifyContent: 'center',
+  },
+  container:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+    paddingBottom: 50,
+  },
+  input:{
+    backgroundColor: '#fff',
+    width: '90%',
+    marginBottom: 15,
+    color: '#222',
+    fontSize: 17,
+    borderRadius: 7,
+    padding: 10,
+  },
+  btnSubmit:{
+    backgroundColor: '#5dd9c0',
+    width: '90%',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 7
+  },
+  submitText:{
+    color: '#fff',
+    fontSize: 18
+  },
+  btnRegister:{
+    marginTop: 10,
+  },
+  registerText:{
+    color: '#fff'
+  }
 });
